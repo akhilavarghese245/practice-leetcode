70a6fbe83a0c37222a3ba7990c37c147b9774bb9
akhila <akhilavarghese174@gmail.com>
2024-10-01 22:22:09 +0530

    Added leetcode qz 1342

---
+++ 1342_steps_reduce_num_zero.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def numberOfSteps(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        count = 0
+        while num != 0:
+            count+=1
+            if num%2 == 0:
+                num = num / 2
+                
+            else:
+                num = num - 1
+        return count
+        
+
+solution = Solution()
+num = 14
+
+print(solution.numberOfSteps(num))
